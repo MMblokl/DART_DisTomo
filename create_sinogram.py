a7d4710c2bd1d9b657c0ef8bba87bad7384a3159
RJC100 <205520702+RJC100@users.noreply.github.com>
2026-05-07 15:14:44 +0200

    Create create_sinogram.py
    Create the create_sinogram.py script that  takes an image, number of detectors and number of projections to create a sinogram representation of the image.

---
+++ create_sinogram.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import astra
+import numpy as np
+import glob
+
+def create_sinogram(img, n_detectors, n_projections):
+    # Put image into 2d data object
+    vol_geom = astra.create_vol_geom(*img.shape) 
+    data_id = astra.data2d.create('-vol', vol_geom, img)
+        
+    # Projections using np.linspace, cutting off the last element of np.pi
+    proj_geom = astra.create_proj_geom('parallel', 1.0, n_detectors, np.linspace(0, np.pi, n_projections + 1)[:-1])
+    proj_id = astra.create_projector('cuda', proj_geom, vol_geom)
+        
+    # Create the sinogram
+    sino_id, sino = astra.create_sino(data_id, proj_id)
+
+    # Clean up of data
+    astra.data2d.delete([sino_id, data_id])
+    astra.projector.delete(proj_id)
+
+    return proj_geom, sino
