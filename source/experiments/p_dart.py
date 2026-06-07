91e8d5c511dcd141355480d32a588290db8e3d05
MMblokl <mats@familieblokland.nl>
2026-06-07 14:40:24 +0200

    p-val experimentation

--- source/experiments/p_dart.py
+++ source/experiments/p_dart.py
@@ -8,14 +8,25 @@ import json
 
 ps = [0.1, 0.2, 0.4, 0.8]
 phantoms = ["blob", "bone", "mesh"]
+detector_numbers = [512, 128, 64]
 
 # Test DART
-resuts = {
+results = {
     phantom: {
-        p_val: {"rnmp": [], "ssim": []} for p_val in ps
+        p_val: {
+            det: {"rnmp": [], "ssim": []} for det in detector_numbers
+            } for p_val in ps
+    } for phantom in phantoms
+}
+final_results = {
+    phantom: {
+        p_val: {
+            det: {"rnmp": 0, "ssim": 0} for det in detector_numbers
+            } for p_val in ps
     } for phantom in phantoms
 }
 
+breakpoint()
 for phantom_group in glob.glob("./phantoms/*"):
     for phantom in glob.glob(f"{phantom_group}/*.png"):
         if "blob" in phantom:
@@ -30,27 +41,35 @@ for phantom_group in glob.glob("./phantoms/*"):
         img = Image.open(phantom)
         img = np.asarray(img)
 
-        proj_geom, sino = create_sinogram(img, 512, 180)
-
-        dart = DART(
-            proj_geom=proj_geom,
-            sinogram=sino,
-            img_shape=img.shape,
-            supersampling_a=1,
-            reconstruction_iterations=100,
-        )
-
         for p_val in ps:
-            rec_img = dart.run(p=p_val, gray_intensities=grey_intensities, iterations=100)
-            rnmp = calc_rnmp(img, rec_img)
-            ssim = calc_ssim(img, rec_img)
+            for n_detectors in detector_numbers:
+                proj_geom, sino = create_sinogram(img, n_detectors, 180)
 
-            resuts[p_group][p_val]["rnmp"].append(rnmp)
-            resuts[p_group][p_val]["ssim"].append(ssim)
+                dart = DART(
+                    proj_geom=proj_geom,
+                    sinogram=sino,
+                    img_shape=img.shape,
+                    supersampling_a=1,
+                    reconstruction_iterations=100,
+                )
+            
+                rec_img = dart.run(p=p_val, gray_intensities=grey_intensities, iterations=100)
+                rnmp = calc_rnmp(img, rec_img)
+                ssim = calc_ssim(img, rec_img)
+
+                results[p_group][p_val][n_detectors]["rnmp"].append(rnmp)
+                results[p_group][p_val][n_detectors]["ssim"].append(ssim)
     
         print(f"Finished with phantom {phantom}.")
     print(f"Finished with phantom family {phantom_group}.")
 
 
+for phantom_group in results.keys():
+    for p_val in results[phantom].keys():
+        for n_detectors in results[phantom_group][p_val].keys():
+            final_results[phantom_group][p_val][n_detectors]["rnmp"] = np.mean(results[phantom_group][p_val][n_detectors]["rnmp"])
+            final_results[phantom_group][p_val][n_detectors]["ssim"] = np.mean(results[phantom_group][p_val][n_detectors]["ssim"])
+
+
 with open("p_dart_results.json", "w") as f:
-    json.dump(resuts, f) 
+    json.dump(final_results, f)
--- source/experiments/visualize_algs.py
+++ source/experiments/visualize_algs.py
@@ -1,1 +1,2 @@
 
+# Show off visual results of each alg and possibly internal pre-segmentation, only border or only free_pix values.
