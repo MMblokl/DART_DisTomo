43b826c57f2fc9e9a2309f6f1e3e12ac67d837bd
MMblokl <mats@familieblokland.nl>
2026-06-08 15:18:27 +0200

    Changed lambda and p experiment routines

--- source/experiments/p_dart.py
+++ source/experiments/p_dart.py
@@ -12,17 +12,11 @@ detector_numbers = [512, 128, 64]
 
 # Test DART
 results = {
-    phantom: {
-        p_val: {
-            det: {"rnmp": [], "ssim": []} for det in detector_numbers
-            } for p_val in ps
+    phantom: { p_val: {{"rnmp": [], "ssim": []} } for p_val in ps
     } for phantom in phantoms
 }
 final_results = {
-    phantom: {
-        p_val: {
-            det: {"rnmp": 0, "ssim": 0} for det in detector_numbers
-            } for p_val in ps
+    phantom: { p_val: {{"rnmp": [], "ssim": []} } for p_val in ps
     } for phantom in phantoms
 }
 
@@ -41,23 +35,22 @@ for phantom_group in glob.glob("./phantoms/*"):
         img = np.asarray(img)
 
         for p_val in ps:
-            for n_detectors in detector_numbers:
-                proj_geom, sino = create_sinogram(img, n_detectors, 180)
+            proj_geom, sino = create_sinogram(img, 64, 180)
 
-                dart = DART(
-                    proj_geom=proj_geom,
-                    sinogram=sino,
-                    img_shape=img.shape,
-                    supersampling_a=1,
-                    reconstruction_iterations=100,
-                )
+            dart = DART(
+                proj_geom=proj_geom,
+                sinogram=sino,
+                img_shape=img.shape,
+                    upersampling_a=1,
+                reconstruction_iterations=100,
+            )
             
-                rec_img = dart.run(p=p_val, gray_intensities=grey_intensities, iterations=100)
-                rnmp = calc_rnmp(img, rec_img)
-                ssim = calc_ssim(img, rec_img)
+            rec_img = dart.run(p=p_val, gray_intensities=grey_intensities, iterations=100)
+            rnmp = calc_rnmp(img, rec_img)
+            ssim = calc_ssim(img, rec_img)
 
-                results[p_group][p_val][n_detectors]["rnmp"].append(rnmp)
-                results[p_group][p_val][n_detectors]["ssim"].append(ssim)
+            results[p_group][p_val]["rnmp"].append(rnmp)
+            results[p_group][p_val]["ssim"].append(ssim)
     
         print(f"Finished with phantom {phantom}.")
     print(f"Finished with phantom family {phantom_group}.")
@@ -65,9 +58,8 @@ for phantom_group in glob.glob("./phantoms/*"):
 
 for phantom_group in results.keys():
     for p_val in results[phantom_group].keys():
-        for n_detectors in results[phantom_group][p_val].keys():
-            final_results[phantom_group][p_val][n_detectors]["rnmp"] = np.mean(results[phantom_group][p_val][n_detectors]["rnmp"])
-            final_results[phantom_group][p_val][n_detectors]["ssim"] = np.mean(results[phantom_group][p_val][n_detectors]["ssim"])
+        final_results[phantom_group][p_val]["rnmp"] = np.mean(results[phantom_group][p_val]["rnmp"])
+        final_results[phantom_group][p_val]["ssim"] = np.mean(results[phantom_group][p_val]["ssim"])
 
 
 with open("p_dart_results.json", "w") as f:
